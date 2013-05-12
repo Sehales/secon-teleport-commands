ce6db4c3dbc3c646b8d26421f3cdd64d357cc4ab
sehales <jan@sehales.net>
2013-05-12 21:22:24 +0200

    removed craftbukkit.jar

---
+++ SeCon-Teleport-Commands/src/net/sehales/scteleportcmds/RequestHandler.java
@@ -0,0 +1,10 @@
+package net.sehales.scteleportcmds;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestHandler {
+
+	private static Map<String, Request> tpRequestMap = new HashMap<String, Request>();
+
+}
